56b28bd809f92b555be1006683513bf61aa3ed13
EngiBeerHub <engibeer0422@gmail.com>
2025-08-09 16:06:40 +0900

    [4-12]Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import {Home} from "@/pages/Home.tsx";
+import NoteDetail from "@/pages/NoteDetail.tsx";
+import Signin from "@/pages/Signin.tsx";
+import Signup from "@/pages/Signup.tsx";
+
 function App() {
-  return (
-    <>
-      <p>hello world</p>
-    </>
-  );
+    return <BrowserRouter>
+        <div className="h-full">
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route index element={<Home/>}></Route>
+                    <Route path={"/notes/:id"} element={<NoteDetail/>}></Route>
+                </Route>
+                <Route path={"/signin"} element={<Signin />}></Route>
+                <Route path={"/signup"} element={<Signup />}></Route>
+            </Routes>
+        </div>
+    </BrowserRouter>;
 }
 
 export default App;
