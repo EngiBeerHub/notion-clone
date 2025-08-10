f477cb8ada228b6feaf68016f605e54ae7990aa7
EngiBeerHub <engibeer0422@gmail.com>
2025-08-10 16:17:11 +0900

    [5-30]define find

--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -6,10 +6,21 @@ export const noteRepository = {
             {
                 user_id: userId,
                 title: params.title,
-                parent_document: params.parentId,
+                parent_document: params.parentId
             }
         ]).select().single();
         if (error != null) throw new Error(error.message);
         return data;
     },
-}
+    async find(userId: string, parentDocumentId: number) {
+        const query = supabase
+            .from('notes')
+            .select()
+            .eq('user_id', userId)
+            .order('created_at', {ascending: false});
+        const {data} = parentDocumentId != null
+            ? await query.eq('parent_document', parentDocumentId)
+            : await query.is('parent_document', null);
+        return data;
+    }
+};
