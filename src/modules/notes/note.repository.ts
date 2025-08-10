af40ace7e64c96e7422df62e165face6157e65e2
EngiBeerHub <engibeer0422@gmail.com>
2025-08-10 14:33:40 +0900

    [5-27]create note

---
+++ src/modules/notes/note.repository.ts
@@ -0,0 +1,15 @@
+import {supabase} from "@/lib/supabase.ts";
+
+export const noteRepository = {
+    async create(userId: string, params: { title?: string; parentId?: number; }) {
+        const {data, error} = await supabase.from('notes').insert([
+            {
+                user_id: userId,
+                title: params.title,
+                parent_document: params.parentId,
+            }
+        ]).select().single();
+        if (error != null) throw new Error(error.message);
+        return data;
+    },
+}
